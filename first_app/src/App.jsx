487abd1dc86f72ba083ba33d84b73a894b6afc02
mchamoudadev <mchamuuda@gmail.com>
2023-08-07 21:16:19 +0300

    list and styles

--- first_app/src/App.jsx
+++ first_app/src/App.jsx
@@ -1,22 +1,9 @@
-import Login from "./Login";
-import Home from "./Home";
+import List from "./List";
 
 export const App = function () {
-	const isLogin = false;
-	const user = "Mc Hamouda";
-	// if (isLogin) {
-	// 	return <Home />;
-	// } else {
-	// 	return <Login />;
-	// }
-
-	// ternary operator
-	// return isLogin ? <Home /> : <Login />;
-
 	return (
 		<>
-			{isLogin && <p>current user is {user}</p>}
-			<h1>Welcome to our website</h1>
+			<List />
 		</>
 	);
 };
